8ea42ff765b252daf4be77b572c3cf8437ed9ea5
Eva Kerrigan <evakerrigan@gmail.com>
2022-08-13 02:19:25 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,29 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+		this.max = max;
+        //this.number = number;
     }
 
     guess() {
+        this.average = Math.round((this.max - this.min) / 2) + this.min;
+        return this.average;
 
+        /*    this.average = Math.round((this.max - this.min) / 2) + this.min;
+            if (this.number === this.average) {return this.average}
+            else if (this.number < this.average) {return this.lower()}
+            else return this.greater();*/		
     }
 
     lower() {
-
+        this.max = this.average;
+        //return this.guess();
     }
 
     greater() {
-
+        this.min = this.average;
+        //return this.guess();
     }
 }
 
